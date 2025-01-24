3ab0f04b9e76894883394d4b86d4816bacfc5fbd
woodycal <72100197+woodycal@users.noreply.github.com>
2025-01-24 12:18:30 +0000

    Create vibration_sensor_calibration.py
    Added tool for calibrating sw-420 sensor

---
+++ test-debug/vibration_sensor_calibration.py
@@ -0,0 +1,26 @@
+#This is for calibrating SW 420 Vibration sensor
+#Run this program while shaking the sensor, you might need to adjust the screw on the sensor to increase or decrease sensityifity.
+
+from machine import Pin
+import time
+
+#Pin setup
+vibration_sensor = Pin(22, Pin.IN)
+
+#Global variables
+vibration_count = 0
+
+#Loop Until 6 vibrations detected then ends loop
+while True:
+    if vibration_sensor.value() == 1:
+        print("Vibration detected!")
+        time.sleep_ms(800)
+        vibration_count = vibration_count + 1
+        if vibration_count == 6:
+            print("alarm trigger")
+            break
+    
+# If no vibration is detected, print ellipses
+    else:
+        print("...")
+        time.sleep_ms(800)
